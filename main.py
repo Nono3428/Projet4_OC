3a001b971f7f16febb1ba5d325f817563aa48279
Arnaud CASSEVILLE <contact.pro.user00@gmail.com>
2024-10-24 11:28:28 +0200

    ajout du main.py qui est le point d'entrer du programme

---
+++ main.py
@@ -0,0 +1,7 @@
+from controllers.main_controller import MainController
+if __name__ == "__main__":
+    # Initialiser le contrôleur principal
+    controller = MainController()
+
+    # Démarrer le programme
+    controller.demarrer()
