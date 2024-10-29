4262b6ce00103cf99c597aaab61f6f6a5f1cac44
Arnaud CASSEVILLE <contact.pro.user00@gmail.com>
2024-10-29 11:49:19 +0100

    mise au propre du fichier joueur_controller

--- controllers/joueurs_controller.py
+++ controllers/joueurs_controller.py
@@ -17,28 +17,6 @@ class JoueursController:
         Joueur.sauvegarder_joueurs(self.joueurs, self.fichier_joueurs)
         Rapport.afficher_message(f"Joueur {nom} {prenom} ajouté avec succès.")
 
-    # def charger_joueurs(self, fichier):
-    #     try:
-    #         with open(fichier, 'r') as f:
-    #             data = f.read()
-    #             if data:
-    #                 joueurs_data = json.loads(data)
-    #                 return [Joueur(**joueur) for joueur in joueurs_data]
-    #             else:
-    #                 Rapport.afficher_message("Le fichier des joueurs est vide. Aucun joueur chargé.")
-    #                 return []
-    #     except FileNotFoundError:
-    #         Rapport.afficher_message("Le fichier des joueurs n'a pas été trouvé.")
-    #         return []
-    #     except json.JSONDecodeError:
-    #         Rapport.afficher_message("Erreur lors de la lecture du fichier JSON des joueurs.")
-    #         return []
-
-    # def sauvegarder_joueurs(self):
-    #     with open(self.fichier_joueurs, 'w') as f:
-    #         json.dump([joueur.to_dict() for joueur in self.joueurs], f, indent=4)
-    #     Rapport.afficher_message("Les joueurs ont été sauvegardés avec succès.")
-    
     def rechercher_joueur(self, identifiant):
         """Recherche un joueur par son identifiant national d'échecs."""
         for joueur in self.joueurs:
