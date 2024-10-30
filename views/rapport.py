098e6ee86e79b3312172d5971bc06c472b869263
Arnaud CASSEVILLE <contact.pro.user00@gmail.com>
2024-10-30 17:38:48 +0100

    ajout de fonction pour afficher le classement ainsi que les tours et les matchs de chaque tour pour le tournoi

--- views/rapport.py
+++ views/rapport.py
@@ -63,14 +63,48 @@ class Rapport:
         print("\n=== Détails du Tournoi ===")
         print(f"Nom : {tournoi.nom}")
         print(f"Lieu : {tournoi.lieu}")
-        print(f"Date de début : {tournoi.date_debut.strftime('%Y-%m-%d')}")
-        print(f"Date de fin : {tournoi.date_fin.strftime('%Y-%m-%d')}")
+        print(f"Date de début : {tournoi.date_debut}")
+        print(f"Date de fin : {tournoi.date_fin}")
         print(f"Nombre de tours : {tournoi.nombre_tours}")
         print(f"Tour courant : {tournoi.tour_actuel}")
         print(f"Description : {tournoi.description}")
-        # print(f"Liste des joueurs : {tournoi.joueurs}")
-        Rapport.afficher_details_joueur(tournoi.joueurs)
+        Rapport.afficher_listes_joueurs(tournoi.joueurs)
 
     @staticmethod
     def afficher_message(message):
         print(message)
+
+    @staticmethod
+    def afficher_classement(tournoi):
+        # Crée une liste des scores à partir du dictionnaire des scores du tournoi
+        classement = sorted(tournoi.joueurs, key=lambda j: tournoi.scores.get(j.identifiant, 0), reverse=True)
+        
+        Rapport.afficher_message("\n--- Classement du Tournoi ---")
+        for index, joueur in enumerate(classement, start=1):
+            # Récupérer le score à partir du dictionnaire des scores
+            score = tournoi.scores.get(joueur.identifiant, 0)
+            Rapport.afficher_message(f"{index}. {joueur.prenom} {joueur.nom} - Score: {score}")
+    
+    @staticmethod
+    def afficher_tours_et_matchs(tournoi):
+        if not tournoi.tours:
+            print("Aucun tour n'a été enregistré pour ce tournoi.")
+            return
+        
+        print(f"\n--- Tours du Tournoi {tournoi.nom} ---")
+        for tour in tournoi.tours:
+            print(f"\nTour {tour.numero}:")
+            if not tour.matchs:
+                print("Aucun match enregistré pour ce tour.")
+            else:
+                for match in tour.matchs:
+                    joueur1 = match.joueur1
+                    joueur2 = match.joueur2
+                    print(f"Match: {joueur1.prenom} {joueur1.nom} vs {joueur2.prenom} {joueur2.nom}")
+                    # Afficher les scores si disponibles
+                    score_joueur1, score_joueur2 = match.score
+                    if score_joueur1 is not None and score_joueur2 is not None:
+                        print(f"Score: {score_joueur1} - {score_joueur2}")
+                    else:
+                        print("Score: Non enregistré.")
+            
